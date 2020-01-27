62a3d2585ea4b7a2769837880dffeb1fe9687fcc
Your Name <you@example.com>
2020-01-27 14:48:36 -0300

    .

--- Resources/views/pdf/tables/tr.blade.php
+++ Resources/views/pdf/tables/tr.blade.php
@@ -5,7 +5,7 @@
 	</td>
 	@endif
 	<td class="border-bottom border-dark p-2">{{ $item->item_product->sku }}</td>
-	<td class="border-bottom border-dark p-2">{{ $item->item_product->description }}</td>
+	<td class="border-bottom border-dark p-2">fsdfs{{ $item->item_product->description }}</td>
 	<td class="border-bottom border-dark text-center p-2">@currency($item->price)</td>
 	@if($setting_pdf->show_item_discount)
 	<td class="border-bottom border-dark text-center p-2">@currency($item->discount_value)</td>
