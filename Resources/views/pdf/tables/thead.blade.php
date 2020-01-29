b0840d5f1c798431171d42fe8dfe536bbc63f263
Your Name <you@example.com>
2020-01-28 23:46:39 -0300

    .

--- Resources/views/pdf/tables/thead.blade.php
+++ Resources/views/pdf/tables/thead.blade.php
@@ -6,7 +6,7 @@
 		<th class="border-bottom border-top border-dark {{ (!$setting_pdf_image->show)?'border-left':'' }} p-2">Ref</th>
 		<th class="border-bottom border-top border-dark p-2">Descrição</th>
 		@loader(['loader_path' => 'pdf.items.thead'])
-		<th class="border-bottom border-top border-dark p-2 text-center">Preço</th>
+		<th class="border-bottom border-top border-dark p-2 text-center">Prç Bru</th>
 		@if($setting_pdf_discount->show)
 		<th class="border-bottom border-top border-dark p-2 text-center">Desc</th>
 		@endif
@@ -16,6 +16,7 @@
 		@if($setting_pdf_taxes->show)
 		<th class="border-bottom border-top border-dark p-2 text-center">Impostos</th>
 		@endif
+		<th class="border-bottom border-top border-dark p-2 text-center">Prç Liq</th>
 		<th class="border-bottom border-top border-dark p-2 text-center">Quant</th>
 		<th class="border-bottom border-top border-dark border-right p-2 text-center">Total</th>
 	</tr>
