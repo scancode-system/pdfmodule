09bb0a69c0729302df0715aa6fc0b20388444369
Your Name <you@example.com>
2020-06-14 16:33:26 -0300

    .

--- Providers/PdfServiceProvider.php
+++ Providers/PdfServiceProvider.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Factory;
 class PdfServiceProvider extends ServiceProvider
 {
     /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'Pdf';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'pdf';
+    /**
      * Boot the application events.
      *
      * @return void
@@ -37,7 +46,17 @@ class PdfServiceProvider extends ServiceProvider
      */
     public function registerViews()
     {
-        $viewPath = resource_path('views/modules/pdf');
+        $viewPath = resource_path('views/modules/' . $this->moduleNameLower);
+
+        $sourcePath = module_path($this->moduleName, 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ], ['views', $this->moduleNameLower . '-module-views']);
+
+        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
+
+        /*$viewPath = resource_path('views/modules/pdf');
 
         $sourcePath = __DIR__.'/../Resources/views';
 
@@ -47,7 +66,7 @@ class PdfServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(array_merge(array_map(function ($path) {
             return $path . '/modules/pdf';
-        }, \Config::get('view.paths')), [$sourcePath]), 'pdf');
+        }, \Config::get('view.paths')), [$sourcePath]), 'pdf');*/
     }
 
 
@@ -59,5 +78,16 @@ class PdfServiceProvider extends ServiceProvider
     public function provides()
     {
         return [];
+    }
+
+    private function getPublishableViewPaths(): array
+    {
+        $paths = [];
+        foreach (\Config::get('view.paths') as $path) {
+            if (is_dir($path . '/modules/' . $this->moduleNameLower)) {
+                $paths[] = $path . '/modules/' . $this->moduleNameLower;
+            }
+        }
+        return $paths;
     }
 }
